18491fc4d8acc94944ea2ed14fe1882bd24b08ff
Rafael Alfaro <alfa0015.rafael@gmail.com>
2018-05-26 17:52:25 -0600

    add fetch listener return promise

--- build/service-worker-prod.js
+++ build/service-worker-prod.js
@@ -52,4 +52,49 @@
         });
       }
   });
+  // addEventListener('fetch', function(event) {
+  //   event.respondWith(
+  //     caches.match(event.request)
+  //       .then(function(response) {
+  //         if (response) {
+  //           return response;
+  //         } else {
+  //           return fetch(event.request)
+  //             .then(function(res) {
+  //               return caches.open(CACHE_DYNAMIC_NAME)
+  //                 .then(function(cache) {
+  //                   cache.put(event.request.url, res.clone());
+  //                   return res;
+  //                 })
+  //             })
+  //             .catch(function(err) {
+  //               return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+  //                 .then(function(cache) {
+  //                   return cache.match('/offline.html');
+  //                 });
+  //             });
+  //         }
+  //       })
+  //   );
+  // });
+  self.addEventListener('fetch', (event) => {
+    console.info('Event: Fetch');
+    var request = event.request;
+    event.respondWith(
+      caches.match(request).then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(request).then((response) => {
+          var responseToCache = response.clone();
+          caches.open(cacheName).then((cache) => {
+            cache.put(request, responseToCache).catch((err) => {
+              console.warn(request.url + ': ' + err.message);
+            });
+          });
+          return response; 
+        });
+      })
+    );
+  });
 })();
