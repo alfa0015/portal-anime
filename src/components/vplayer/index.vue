61d0952416bf1fbe345a232e85950729da553ebd
Rafael Alfaro <alfa0015.rafael@gmail.com>
2019-03-01 23:50:34 -0600

    add video player anime

---
+++ src/components/vplayer/index.vue
@@ -0,0 +1,24 @@
+<template src="./template.pug" lang="pug">
+</template>
+
+<script>
+export default {
+  name: 'vplayer',
+  props: ['video', 'banner'],
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  mounted () {
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
