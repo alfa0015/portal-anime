3b9916e0dcd516abbcb26252810029d17ad5c59a
Rafael Alfaro <alfa0015.rafael@gmail.com>
2019-03-14 22:42:33 -0600

    add force https to express

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
 const compression = require('compression');
+const secure = require('express-force-https');
 
 let app = express();
-app.use(compression(),serveStatic(__dirname + "/dist"));
+app.use(secure,compression(),serveStatic(__dirname + "/dist"));
+
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html')
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
