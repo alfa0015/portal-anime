edbdea5648251cfa1dc241d8f80888b52105e68c
Rafael Alfaro <alfa0015.rafael@gmail.com>
2019-03-14 22:16:00 -0600

    add perfomance config and set language es

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
+const compression = require('compression');
 
 let app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(compression(),serveStatic(__dirname + "/dist"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
