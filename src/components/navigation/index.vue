08099ea58bfc184fe5f106a3b6a0d4bd99064976
alfa0015 <alfa0015.rafael@gmail.com>
2019-04-19 13:36:18 -0600

    navigation drawner complet links and links to episodes

--- src/components/navigation/index.vue
+++ src/components/navigation/index.vue
@@ -1,105 +1,47 @@
- <template>
-	<div>
-			<v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" fixed app>
-			<v-list dense>
-        <template v-for="item in items">
-          <v-layout
-              v-if="item.heading"
-              :key="item.heading"
-              row
-              align-center
-          >
-              <v-flex xs6>
-              <v-subheader v-if="item.heading">
-                  {{ item.heading }}
-              </v-subheader>
-              </v-flex>
-              <v-flex xs6 class="text-xs-center">
-              <a href="#!" class="body-2 black--text">EDIT</a>
-              </v-flex>
-          </v-layout>
-          <v-list-group
-              v-else-if="item.children"
-              :key="item.text"
-              v-model="item.model"
-              :prepend-icon="item.model ? item.icon : item['icon-alt']"
-              append-icon=""
-          >
-              <template v-slot:activator>
-                <v-list-tile>
-                    <v-list-tile-content>
-                    <v-list-tile-title>
-                        {{ item.text }}
-                    </v-list-tile-title>
-                    </v-list-tile-content>
-                </v-list-tile>
-              </template>
-              <v-list-tile
-              v-for="(child, i) in item.children"
-              :key="i"
-              @click="void(0)"
-              >
-              <v-list-tile-action v-if="child.icon">
-                  <v-icon>{{ child.icon }}</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                  <v-list-tile-title>
-                  {{ child.text }}
-                  </v-list-tile-title>
-              </v-list-tile-content>
-              </v-list-tile>
-          </v-list-group>
-          <v-list-tile v-else :key="item.text" @click="void(0)">
-              <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-              <v-list-tile-title>
-                  {{ item.text }}
-              </v-list-tile-title>
-              </v-list-tile-content>
-          </v-list-tile>
-				</template>
-			</v-list>
-			</v-navigation-drawer>
-			<v-toolbar :clipped-left="$vuetify.breakpoint.lgAndUp" color="blue darken-3" dark app fixed>
-			<v-toolbar-title style="width: 300px" class="ml-0 pl-3">
-					<v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-					<span class="hidden-sm-and-down">Portal Anime</span>
-			</v-toolbar-title>
-			<v-text-field
-					flat
-					solo-inverted
-					hide-details
-					prepend-inner-icon="search"
-					label="Search"
-					class="hidden-sm-and-down"
-			></v-text-field>
-			<v-spacer></v-spacer>
-			<v-btn icon>
-					<v-icon>apps</v-icon>
-			</v-btn>
-			<v-btn icon>
-					<v-icon>notifications</v-icon>
-			</v-btn>
-			<v-btn icon large>
-					<v-avatar size="32px" tile>
-					<img
-							src="https://cdn.vuetifyjs.com/images/logos/logo.svg"
-							alt="Vuetify"
-					>
-					</v-avatar>
-			</v-btn>
-			</v-toolbar>
-	</div>
+<template lang="pug">
+	div
+		v-navigation-drawer(v-model='drawer', :clipped='$vuetify.breakpoint.lgAndUp', fixed='', app='')
+			v-list.pt-0(dense='')
+				v-list-tile(@click='', :to="{path: '/'}")
+					v-list-tile-action
+						v-icon home
+					v-list-tile-content
+						v-list-tile-title Home
+				v-list-tile(@click='', :to="{path: '/emision'}")
+					v-list-tile-action
+						v-icon local_movies
+					v-list-tile-content
+						v-list-tile-title Emision
+				v-list-tile(@click='', :to="{path: '/foro'}")
+					v-list-tile-action
+						v-icon message
+					v-list-tile-content
+						v-list-tile-title Foro
+				v-list-tile(@click='', :to="{path: '/account'}")
+					v-list-tile-action
+						v-icon account_circle
+					v-list-tile-content
+						v-list-tile-title Mi cuenta
+		v-toolbar(:clipped-left='$vuetify.breakpoint.lgAndUp', color='blue darken-3', dark='', app='', fixed='')
+			v-toolbar-title.ml-0.pl-3(style='width: 300px;')
+				v-toolbar-side-icon(@click.stop='drawer = !drawer')
+				span.hidden-sm-and-down Portal Anime
+			v-text-field.hidden-sm-and-down(flat='', solo-inverted='', hide-details='', prepend-inner-icon='search', label='Buscar')
+			v-spacer
+			v-btn(icon='')
+				v-icon apps
+			v-btn(icon='')
+				v-icon notifications
+			v-btn(icon='', large='')
+				v-avatar(size='32px', tile='')
+					img(src='https://cdn.vuetifyjs.com/images/logos/logo.svg', alt='Vuetify')
 </template>
-
 <script>
 export default {
-  name: 'navigation',
-  components: {
-  },
-  data: () => ({
+	name: 'navigation',
+	components: {
+	},
+	data: () => ({
 		drawer: null,
 		items: [
 			{ icon: 'home', text: 'Home' },
@@ -110,3 +52,4 @@ export default {
 	})
 }
 </script>
+
